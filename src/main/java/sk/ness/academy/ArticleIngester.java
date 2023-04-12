f64853f7b502abbe705c756a6ebd9426542e2771
vudmaska <vudmaska@gmail.com>
2023-04-12 14:56:39 +0200

    Changed path to a file

--- src/main/java/sk/ness/academy/ArticleIngester.java
+++ src/main/java/sk/ness/academy/ArticleIngester.java
@@ -25,7 +25,7 @@ public class ArticleIngester {
 
       // Load file with articles and ingest
 
-      articleService.ingestArticles("D:\\Java\\zadanie\\articles_to_ingest.txt");
+      articleService.ingestArticles(System.getProperty("user.dir") + "\\articles_to_ingest.txt");
     }
   }
 }
